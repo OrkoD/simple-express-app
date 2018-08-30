eb5b84420b025b3bba801aa4200bf8d0313e7aac
Orest <orko1986@gmail.com>
2018-08-30 13:16:19 +0300

    delete some comments in index.js fils

--- index.js
+++ index.js
@@ -5,15 +5,4 @@ const debug = require(`debug`)(`app:debug`);
 
 require(`./startup/db`)();
 
-// const { User } = require(`./models/user`);
-// const user = new User({
-//   name: `Mrs. Dennis Schulist`,
-//   email: `Julianne.OConner@kory.or`,
-//   phone: `493-170-9623 x156`,
-//   catchPhrase: `Multi-tiered zero tolerance productivity`,
-//   company: `Robel-Corkery`,
-//   imagePath: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOcvv2odddxPP5AcMEq74E5xl5r55nxE5gEYz88ThR8W0w_uNv`
-// });
-// user.save();
-
 app.listen(port, () => debug(`Magic is happening on port: ${port}...`));
